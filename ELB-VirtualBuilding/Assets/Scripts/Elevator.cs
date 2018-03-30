fd06bd9e6414782274bf08e2594d5eccc615f299
Aristotelis Papaioannou <adpapaio@gmail.com>
2018-03-30 14:02:49 -0400

    Elevator changes

---
+++ ELB-VirtualBuilding/Assets/Scripts/Elevator.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Elevator : MonoBehaviour {
+
+	public int floorCount;
+	public Vector3[] Levels;
+	private int currentFloor;
+
+	// Use this for initialization
+	void Start ()
+	{
+		currentFloor = 1;
+		this.gameObject.transform.position = Levels[0];
+	}
+
+	// Update is called once per frame
+	void Update () 
+	{
+		
+	}
+
+	public void MoveElevator(int floor)
+	{
+		transform.position = Vector3.Lerp(Levels[currentFloor-1], Levels[floor-1], 0f);
+	}
+}
