35c03c5e074023ff2ea090de4d2ec0b184545e13
Aristotelis Papaioannou <adpapaio@gmail.com>
2018-04-14 14:47:09 -0400

    Created and added the animation for the Sliding door

---
+++ ELB-VirtualBuilding/Assets/Scripts/ClosingOpeningDoors.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClosingOpeningDoors : MonoBehaviour {
+
+	public Animator animator;
+
+	public void Start()
+	{
+		animator = gameObject.GetComponent<Animator>();
+	}
+	public void OnTriggerEnter(Collider col)
+	{
+		if(animator.GetBool("isOpening"))
+		{
+			animator.SetBool("isOpening", false);
+		}
+		else
+		{
+			animator.SetBool("isOpening", true);
+		}
+		
+
+	}
+}
