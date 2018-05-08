9fa04c5cd723504fc8eb3393a29fd771d456389a
Aristotelis Papaioannou <adpapaio@gmail.com>
2018-05-08 14:54:46 -0400

    Created Button Prefab, Edited Project settings, other minor things

---
+++ ELB-VirtualBuilding/Assets/Button.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Button : MonoBehaviour {
+
+	public AudioSource audio;
+	
+	public void OnTriggerEnter(Collider collider)
+	{
+		if(!audio.isPlaying)
+		{
+			audio.Play();
+		}
+	}
+	
+}
