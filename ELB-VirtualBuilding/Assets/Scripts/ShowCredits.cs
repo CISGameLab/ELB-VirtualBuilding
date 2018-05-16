f1af982f4ba34723a83ab8cad6f1c6aaf8e235fa
aiseblin <aiseblin@umich.edu>
2018-05-16 12:26:49 -0400

    credit screen made

---
+++ ELB-VirtualBuilding/Assets/Scripts/ShowCredits.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShowCredits : MonoBehaviour {
+    public GameObject credits;
+    private bool isShowing;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            isShowing = !isShowing;
+            credits.SetActive(isShowing);
+        }
+	}
+}
