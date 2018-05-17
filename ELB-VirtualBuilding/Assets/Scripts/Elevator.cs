673ca3868a0fab5204d87ba9a6adc520c637e40d
Aristotelis Papaioannou <adpapaio@gmail.com>
2018-05-17 13:09:28 -0400

    Fixed everything
    For some reason the work done yesterday was lost, I fixed it.

--- ELB-VirtualBuilding/Assets/Scripts/Elevator.cs
+++ ELB-VirtualBuilding/Assets/Scripts/Elevator.cs
@@ -6,33 +6,12 @@ public class Elevator : MonoBehaviour {
 
     public GameObject player;
     public Transform spawnPoints;
-	public int floorCount;
-	public Vector3[] Levels;
-	private int currentFloor;
 
-	// Use this for initialization
-	void Start ()
-	{
-		currentFloor = 1;
-		this.gameObject.transform.position = Levels[0];
-	}
-
-	// Update is called once per frame
-	void Update () 
-	{
-		
-	}
-
-	public void MoveElevator(int floor)
-	{
-		transform.position = Vector3.Lerp(Levels[currentFloor-1], Levels[floor-1], 0f);
-	}
-
-    void OnTriggerEnter(Collision col)
+    void OnTriggerEnter(Collider col)
     {
-        if(col.transform.tag == "FirstPersonCharacter")
+        if(col.transform.tag == "Player")
         {
-            player = col.transform.parent.gameObject;
+            player = col.transform.gameObject;
             player.transform.position = spawnPoints.transform.position;
             player.transform.rotation = spawnPoints.transform.rotation;
         }
